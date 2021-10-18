5e9d075f476a355fcaf7ee7c2eae32876ca2de9c
Ana <ana.cluh@hotmail.com>
2021-10-18 15:59:18 -0300

    criando nova classe de negocio

--- src/main/java/br/com/ana/projeto_integrador/modelo/Produto.java
+++ src/main/java/br/com/ana/projeto_integrador/modelo/Produto.java
@@ -1,5 +1,10 @@
 package br.com.ana.projeto_integrador.modelo;
 
 public class Produto {
-
+	
+	private long Id;
+	private String nome;
+	private String quantide;
+	private String descrição;
+	private double valor;
 }
